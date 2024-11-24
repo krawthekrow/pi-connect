f2fc271d34095335b5677f04cfffee1e6a56b6ae
Nathan Pinsker <npinsker@gmail.com>
2024-11-23 21:29:07 -0800

    add ability to vary size of connections & sequences round

--- src/js/components/Choose.jsx
+++ src/js/components/Choose.jsx
@@ -5,30 +5,37 @@ import GardinerFont from '../../fonts/Gardiner-reduced.ttf';
 
 const ChoosePanel = (props) => {
 	const rows = [];
-	for (let i = 0; i < 2; i++) {
+
+	let numColumns = (props.numChoices == 2) ? 1 : 3;
+	let numRows = Math.ceil(props.numChoices / numColumns);
+
+	for (let i = 0; i < numRows; i++) {
 		const row = [];
-		for (let j = 0; j < 3; j++) {
-			if (props.numChoices == 2 && (j == 0 || j == 2))
-				continue;
-			const index = i * 3 + j;
-			const chosenIndex =
-				(props.numChoices == 2) ? (index - 1) / 3 : index;
-			const disabled = props.chosen.includes(chosenIndex);
-			const handleClick = ((index, disabled) => ((e) => {
+		for (let j = 0; j < numColumns; j++) {
+			const index = i * numColumns + j;
+			if (index >= props.numChoices) {
+				break;
+			}
+			const disabled = props.chosen.includes(index);
+			const handleClick = (e) => {
 				e.target.blur();
 				props.onClick(index);
-			}))(chosenIndex, disabled);
+			};
 			row.push(
-			<div key={j} className="col p-0 m-1">
-				<button type="button" className={`btn btn-dark m-0 p-0 w-100${disabled ? ' disabled' : ''}`} onClick={handleClick} style={{
-					backgroundColor: 'navy'
-				}}>
-					<h1 style={{
-						fontSize: '100px',
-						fontFamily: 'Gardiner'
-					}}>{ChoosePanel.HIEROGLYPHS[index]}</h1>
-				</button>
-			</div>
+				<div key={j} className="col p-0 m-1" style={{maxWidth: (numColumns == 3 ? 'calc(33% - .5rem)' : undefined)}}>
+					<button
+					 type="button"
+					 className={`btn btn-dark m-0 p-0 w-100${disabled ? ' disabled' : ''}`}
+					 onClick={handleClick}
+					 style={{
+						backgroundColor: 'navy'
+					 }}>
+						<h1 style={{
+							fontSize: '100px',
+							fontFamily: 'Gardiner'
+						}}>{ChoosePanel.HIEROGLYPHS[index % ChoosePanel.HIEROGLYPHS.length]}</h1>
+					</button>
+				</div>
 			);
 		}
 		rows.push(
@@ -42,8 +49,7 @@ const ChoosePanel = (props) => {
 		<div className="container">
 			<h1>{props.header}</h1>
 		</div>
-		{rows[0]}
-		{rows[1]}
+		{rows}
 	</div>
 	];
 };
@@ -59,7 +65,15 @@ ChoosePanel.HIEROGLYPHS = [
 	'\ud80c\udf9b',
 	'\ud80c\udd91',
 	'\ud80c\ude17',
-	'\ud80c\udc80'
+	'\ud80c\udc80',
+
+	// Extended set... picked randomly from the set of Unicode hieroglyphs
+	'\uD80C\uDCFD',
+	'\uD80C\uDF70',
+	'\uD80C\uDD03',
+	'\uD80C\uDDEC',
+	'\uD80C\uDDD6',
+	'\uD80C\uDF8B',
 ];
 
 export default ChoosePanel;
